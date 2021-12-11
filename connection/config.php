f77300c94d6026c1c1c77c7b2111471d02f50564
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-12-11 01:56:16 +0000

    Files

---
+++ connection/config.php
@@ -0,0 +1,15 @@
+<?php
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "peakflow_tms";
+
+    $conn = new mysqli($server, $username, $password, $dbname);
+
+    // if($conn)
+    // {
+    //     echo "Connection Success";
+    // }
+
+?>
